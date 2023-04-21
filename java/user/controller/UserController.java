92782283806d2cc3827e7ee1767f0051f035ccbc
gkdltjdrms <121652277+gkdltjdrms@users.noreply.github.com>
2023-04-21 18:22:30 +0900

    Add files via upload

---
+++ java/user/controller/UserController.java
@@ -0,0 +1,52 @@
+package user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="user")
+public class UserController {
+	
+	@GetMapping(value="playvideo")
+	public String playvideo(Model model) {
+		model.addAttribute("display","");
+		model.addAttribute("display3", "./video/playvideo.jsp");
+		return "index";
+	}
+	
+	@GetMapping(value="channelForm")
+	public String channelForm(Model model) {
+		
+		model.addAttribute("display","./channel/channelForm.jsp");
+		return "index";
+	}
+	@GetMapping(value="uploadForm")
+	public String uploadForm(Model model) {
+		
+		model.addAttribute("display","./channel/uploadForm.jsp");
+		return "index";
+	}
+	
+	@GetMapping(value="boardList")
+	public String boardList(Model model) {
+		
+		model.addAttribute("display","./channel/channelForm.jsp");
+		model.addAttribute("display2","../board/boardList.jsp");
+	
+		return "index";
+	}
+}
+
+
+
+
+
+
+
+
